2ba475e3cb2cee0a5b4bb23abfb28c1b25cab653
davidhu34 <b01901133@gmail.com>
2016-05-30 17:43:02 +0800

    Spawning

---
+++ Assets/EnemyMgr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMgr : MonoBehaviour {
+    public GameObject enemyTank;
+
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Spawn", 5f, 5f);
+    }
+
+
+    void Spawn()
+    {
+        Vector3 SpawnPosition = new Vector3(PosNeg() * Random.Range(35f, 45f), 0f, PosNeg() * Random.Range(35f, 45f));
+        Quaternion SpawnRotation = Quaternion.Euler(0f, Random.Range(-180f, 180f), 0f);
+        Instantiate( enemyTank, SpawnPosition, SpawnRotation);
+    }
+
+    private float PosNeg()
+    {
+        float random = Random.Range(-1f, 1f);
+        if (random < 0) return -1f;
+        else return 1f;
+    }
+}
